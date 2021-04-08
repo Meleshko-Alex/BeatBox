b9508cfff3ff5e121c7ec2dc1a236940d2cd5a01
alex.M <meleha.alex@gmail.com>
2021-04-08 18:32:26 +0300

    Add tests

--- app/src/main/java/com/meleshko/beatbox/SoundViewModel.kt
+++ app/src/main/java/com/meleshko/beatbox/SoundViewModel.kt
@@ -2,14 +2,20 @@ package com.meleshko.beatbox
 
 import androidx.lifecycle.MutableLiveData
 
-class SoundViewModel {
+class SoundViewModel(private val beatBox: BeatBox) {
+
+    fun onButtonClicked() {
+        sound?.let {
+            beatBox.play(it)
+        }
+    }
 
     val title: MutableLiveData<String?> = MutableLiveData()
 
     var sound: Sound? = null
-    set(sound) {
-        field = sound
-        title.postValue(sound?.name)
-    }
+        set(sound) {
+            field = sound
+            title.postValue(sound?.name)
+        }
 
 }
